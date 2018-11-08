acb192a781538b025d3a81d9a830aac0f9398f8d
TARUC <TARUC@PC-98>
2018-11-08 14:32:10 +0800

    testing

--- src/newsample/Newsample.java
+++ src/newsample/Newsample.java
@@ -16,6 +16,8 @@ public class Newsample {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("HEllo world 1");
+        System.out.println("Hello World 2");
     }
     
 }
