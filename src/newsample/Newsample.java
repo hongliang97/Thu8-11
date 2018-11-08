7c9b29487b41674486510708a9a1e87f56827d4c
TARUC <TARUC@PC-98>
2018-11-08 14:39:07 +0800

    testing

--- src/newsample/Newsample.java
+++ src/newsample/Newsample.java
@@ -16,8 +16,9 @@ public class Newsample {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Hello World 1");
         System.out.println("Hello World 2");
-        System.out.println("Hello World 2");
+        System.out.println("Hello World 3");
     }
     
 }
